b68920563481129c5512ac4f51df87bcaebf738e
undefinedtv <undefinedtv9@gmail.com>
2025-11-28 23:51:07 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -1,7 +1,7 @@
 # Birleştirilecek dosya adları
-dosya1 = 'selcuk.m3u'
+dosya1 = 'goals.m3u'
 dosya2 = 'yeni.m3u'
-dosya3 = 'spor-karma.m3u'
+dosya3 = 'tabii.m3u'
 cikis_dosyasi = 'karisik.m3u'
 
 # M3U dosyalarının içeriğini oku
@@ -15,7 +15,7 @@ icerik2 = oku_m3u(dosya2)
 icerik3 = oku_m3u(dosya3)
 
 # Birleştir
-birlesik_icerik = icerik1 + icerik2
+birlesik_icerik = icerik1 + icerik2 + icerik3
 
 # Yeni dosyaya yaz
 with open(cikis_dosyasi, 'w', encoding='utf-8') as f:
