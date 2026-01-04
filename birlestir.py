420d55b430fe428a81d6f21f1ced6ecb9dbcd029
undefinedtv <undefinedtv9@gmail.com>
2026-01-04 22:22:39 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -1,15 +1,15 @@
 # Birleştirilecek dosya adları
 #goals = 'goals.m3u'
 empty = 'empty.m3u'
+karsilasmalar = 'karsilasmalar.m3u'
+rec = 'rec.m3u'
+rec2 = 'rec2.m3u'
 inattv = 'inattv.m3u'
 selcuk = 'selcuk.m3u'
 andro = 'androtv.m3u'
 tabii = 'tabii.m3u'
 yeni = 'yeni.m3u'
 vavoo = 'vavoo.m3u'
-rec = 'rec.m3u'
-rec2 = 'rec2.m3u'
-karsilasmalar = 'karsilasmalar.m3u'
 cikis_dosyasi = 'karisik.m3u'
 
 # M3U dosyalarının içeriğini oku
@@ -19,15 +19,24 @@ def oku_m3u(dosya_adi):
 
 # İçerikleri oku
 empty_icerik = oku_m3u(empty)
-inattv_icerik = oku_m3u(inattv)
-selcuk_icerik = oku_m3u(selcuk)
-andro_icerik = oku_m3u(andro)
-tabii_icerik = oku_m3u(tabii)
-yeni_icerik = oku_m3u(yeni)
-vavoo_icerik = oku_m3u(vavoo)
-rec_icerik = oku_m3u(rec)
-rec2_icerik = oku_m3u(rec2)
+print({empty_icerik})
 karsilasmalar_icerik = oku_m3u(karsilasmalar)
+print({karsilasmalar_icerik})
+rec_icerik = oku_m3u(rec)
+print({rec_icerik})
+rec2_icerik = oku_m3u(rec2)
+print({rec2_icerik})
+inattv_icerik = oku_m3u(inattv)
+print({inattv_icerik})
+selcuk_icerik = oku_m3u(selcuk)
+print({selcuk_icerik})
+andro_icerik = oku_m3u(andro)
+print({andro_icerik})
+tabii_icerik = oku_m3u(tabii)
+print({tabii_icerik})
+yeni_icerik = oku_m3u(yeni)
+print({yeni_icerik})
+vavoo_icerik = oku_m3u(vavoo)
 
 # Birleştir
 birlesik_icerik = empty_icerik + karsilasmalar_icerik + rec_icerik + rec2_icerik + inattv_icerik + selcuk_icerik + andro_icerik + tabii_icerik + yeni_icerik
