dc0d9eb73edbddc647156a9443a3fb5a765a5b1d
undefinedtv <undefinedtv9@gmail.com>
2026-02-01 22:17:39 +0300

    Update gbl.py

--- gbl.py
+++ gbl.py
@@ -6,7 +6,7 @@ from io import BytesIO
 
 # CALMA OC
 
-SOURCE_ORDER = ["smart", "kablo", "boncuktv", "goldvod"]
+SOURCE_ORDER = ["kablo", "smart", "boncuktv", "goldvod"]
 
 OUTPUT_FILENAME = "yeni.m3u"
 
