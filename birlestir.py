43720f05b7b33ce4c16cca97a8559d1773b2d81b
undefinedtv <undefinedtv9@gmail.com>
2026-08-04 21:58:54 +0300

    Update content merging in birlestir.py
    Refactor content merging logic in birlestir.py.

--- birlestir.py
+++ birlestir.py
@@ -52,7 +52,7 @@ legal_icerik = oku_m3u(legal)
 
 # Birleştir
 birlesik_icerik = (
-    empty_icerik + karsilasmalar_icerik + rec_icerik + zz_icerik +
+    empty_icerik + karsilasmalar_icerik  + zz_icerik +
     atom_icerik + selcuk_icerik + andro_icerik + tabii_icerik 
 )
 
